9daffd20d0722519eb4ea5af74c19c52563263d6
bissan <109983081+bissan2@users.noreply.github.com>
2022-11-02 19:13:51 +0300

    done

--- script.js
+++ script.js
@@ -8,20 +8,21 @@ let random_grade_div = document.getElementById("random-grade");
 // لا تقم بتغيير او تخريب الكود الموجود فوق هذا الكومنت
 
 // طبّق الخطوة 4 في أسفل هذا الكومنت
-
+grades.pop();
 // طبّق الخطوة 5 في أسفل هذا الكومنت
-
+grades.push(99);
 // طبّق الخطوة 6 أسفل  هذا الكومنت
-
+grades.forEach((grade) => {
+  all_grades_div.innerHTML += `<div>${grade}</div>`;
+})
 
 
 function search() {
   
   // طبّق الخطوة 7و8 أسفل هذا الكومنت
-  
-  
+  let randomIndex = Math.floor(Math.random() * grades.length);
   // طبّق الخطوة 9 أسفل هذا الكومنت
-
+  random_grade_div.innerHTML = `<div>${grades[randomIndex]}</div>`;
 }
 
 
